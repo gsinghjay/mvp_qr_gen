de22c7ed8a53df296a613b3338f3c1ed094cf072
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2025-05-21 16:13:35 -0400

    refactor(types): update type definitions to use specialized repositories

--- app/types.py
+++ app/types.py
@@ -8,16 +8,15 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from .database import get_db_with_logging
-from .repositories import OriginalQRCodeRepository, QRCodeRepository, ScanLogRepository
+from .repositories import QRCodeRepository, ScanLogRepository
 from .services.qr_service import QRCodeService
-from .dependencies import get_qr_repository, get_qr_service, get_new_qr_repository, get_scan_log_repository
+from .dependencies import get_qr_service, get_qr_code_repository, get_scan_log_repository
 
 # Database session type
 DbSessionDep = Annotated[Session, Depends(get_db_with_logging)]
 
 # Repository types
-QRRepositoryDep = Annotated[OriginalQRCodeRepository, Depends(get_qr_repository)]
-QRCodeRepositoryDep = Annotated[QRCodeRepository, Depends(get_new_qr_repository)]
+QRCodeRepositoryDep = Annotated[QRCodeRepository, Depends(get_qr_code_repository)]
 ScanLogRepositoryDep = Annotated[ScanLogRepository, Depends(get_scan_log_repository)]
 
 # Service types
