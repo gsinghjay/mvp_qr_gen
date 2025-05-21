d9b0b832dfac4d14be2ac8e354a6b6f12ae4f8bc
Jay Singh <54427246+gsinghjay@users.noreply.github.com>
2025-05-21 15:20:02 -0400

    refactor: update type aliases for new repository naming

--- app/types.py
+++ app/types.py
@@ -8,15 +8,17 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from .database import get_db_with_logging
-from .repositories.qr_repository import QRCodeRepository
+from .repositories import OriginalQRCodeRepository, QRCodeRepository, ScanLogRepository
 from .services.qr_service import QRCodeService
-from .dependencies import get_qr_repository, get_qr_service
+from .dependencies import get_qr_repository, get_qr_service, get_new_qr_repository, get_scan_log_repository
 
 # Database session type
 DbSessionDep = Annotated[Session, Depends(get_db_with_logging)]
 
 # Repository types
-QRRepositoryDep = Annotated[QRCodeRepository, Depends(get_qr_repository)]
+QRRepositoryDep = Annotated[OriginalQRCodeRepository, Depends(get_qr_repository)]
+QRCodeRepositoryDep = Annotated[QRCodeRepository, Depends(get_new_qr_repository)]
+ScanLogRepositoryDep = Annotated[ScanLogRepository, Depends(get_scan_log_repository)]
 
 # Service types
 QRServiceDep = Annotated[QRCodeService, Depends(get_qr_service)] 
